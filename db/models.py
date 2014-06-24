8741502eb4dadf278b7cc0b54f469594727c8e20
Petr Devaikin <p.devaikin@gmail.com>
2014-06-24 19:12:28 +0400

    python packages

--- db/models.py
+++ db/models.py
@@ -4,47 +4,64 @@ from peewee import *
 db = MySQLDatabase(None)
 
 class Location(Model):
-	name = CharField()
-	north = DoubleField()
-	south = DoubleField()
-	west = DoubleField()
-	east = DoubleField()
-	height = IntegerField()
-	north_width = IntegerField()
-	south_width = IntegerField()
+    name = CharField()
+    north = DoubleField()
+    south = DoubleField()
+    west = DoubleField()
+    east = DoubleField()
+    height = IntegerField()
+    north_width = IntegerField()
+    south_width = IntegerField()
 
-	class Meta:
-		database = db
+    class Meta:
+        database = db
 
 
 class SimpleArea(Model):
-	location = ForeignKeyField(Location, related_name='simple_areas')
-	latitude = DoubleField()
-	longitude = DoubleField()
-	radius = IntegerField()
-	updated = DateTimeField(null=True)
+    location = ForeignKeyField(Location, related_name='simple_areas')
+    to_north = ForeignKeyField('self', related_name='to_south', null=True)
+    to_west = ForeignKeyField('self', related_name='to_east', null=True)
+    latitude = DoubleField()
+    longitude = DoubleField()
+    radius = IntegerField()
+    updated = DateTimeField(null=True)
 
-	def most_popular_tag(self):
-		if self.hashtag_counts.count() > 0:
-			return self.hashtag_counts.order_by(HashtagFrequency.count.desc()).get()
-		else:
-			return None
+    def most_popular_tag(self):
+        if self.hashtag_counts.count() > 0:
+            return self.hashtag_counts.order_by(HashtagFrequency.count.desc()).get()
+        else:
+            return None
 
-	class Meta:
-		database = db
+
+    def add_connections(self):
+        all_north = SimpleArea.select().where((SimpleArea.longitude == self.longitude) & \
+            (SimpleArea.latitude > self.latitude) & (SimpleArea.location == self.location))
+        if all_north.count() > 0:
+        	self.to_north = all_north.order_by(SimpleArea.latitude).get()
+
+        all_west = SimpleArea.select().where((SimpleArea.latitude == self.latitude) & \
+            (SimpleArea.longitude < self.longitude) & (SimpleArea.location == self.location))
+        if all_west.count() > 0:
+        	self.to_west = all_west.order_by(SimpleArea.longitude.desc()).get()
+
+        self.save()
+
+
+    class Meta:
+        database = db
 
 
 class Hashtag(Model):
-	name = CharField(unique=True)
+    name = CharField(unique=True)
 
-	class Meta:
-		database = db
+    class Meta:
+        database = db
 
 
 class HashtagFrequency(Model):
-	hashtag = ForeignKeyField(Hashtag, related_name='counts')
-	simple_area = ForeignKeyField(SimpleArea, related_name='hashtag_counts')
-	count = IntegerField(default=0)
+    hashtag = ForeignKeyField(Hashtag, related_name='counts')
+    simple_area = ForeignKeyField(SimpleArea, related_name='hashtag_counts')
+    count = IntegerField(default=0)
 
-	class Meta:
-		database = db
+    class Meta:
+        database = db
--- tags_updater.py
+++ tags_updater.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
-import sys
-sys.path.append("db")
-sys.path.append("datagrabber")
-
 import time
-from models import *
-from insta_grabber import *
-from config import *
+import datetime
+from db.models import *
+from datagrabber.insta_grabber import *
+from .config import *
 
-db.init(DB_NAME, user=DB_USER, password=DB_PASSWORD)
 
+def update_tags(time_delta):
+    db.init(DB_NAME, user=DB_USER, password=DB_PASSWORD)
 
-grabber = InstaGrabber(CLIENT_ID, CLIENT_SECRET)
-now = time.time()
+    grabber = InstaGrabber(CLIENT_ID, CLIENT_SECRET)
+    now = time.time()
 
-time_dalta = 3600
+    for area in SimpleArea.select():
+        HashtagFrequency.delete().where(HashtagFrequency.simple_area == area).execute()
 
-for area in SimpleArea.select():
-    HashtagFrequency.delete().where(HashtagFrequency.simple_area == area).execute()
+        tags = grabber.find_tags((area.latitude, area.longitude), area.radius, now, now - time_delta, \
+            COMMON_IGNORE)
 
-    tags = grabber.find_tags((area.latitude, area.longitude), area.radius, now, now - time_dalta, \
-        COMMON_IGNORE)
+        for tag_name in tags:
+            hashtag = Hashtag.get_or_create(name=tag_name)
+            HashtagFrequency.create(simple_area=area, hashtag=hashtag, count=tags[tag_name])
 
-    for tag_name in tags:
-        hashtag = Hashtag.get_or_create(name=tag_name)
-        HashtagFrequency.create(simple_area=area, hashtag=hashtag, count=tags[tag_name])
+        area.updated = datetime.datetime.now
+        area.save()
 
-    print "+++ {2} tags for area {0} of {1} updated".format(area.id, area.location.name, area.hashtag_counts.count())
-    print "Tags count: ", Hashtag.select().count()
+        print "+++ {2} tags for area {0} of {1} updated".format(area.id, area.location.name, area.hashtag_counts.count())
+        print "Tags count: ", Hashtag.select().count()
