043c6b739576cac56002110d558e5928e94b7126
p.devaikin@gmail.com <p.devaikin@gmail.com>
2014-08-25 14:04:45 +0400

    new ignore

--- htmapp/default_settings.py
+++ htmapp/default_settings.py
@@ -10,7 +10,7 @@ COMMON_IGNORE = [u'instasize', u'love',
     u'vsco', u'vscocam', u'vscophoto', u'vscogood', u'vscobest', u'vscodaily', u'vscolike',
     u'vscoonly', u'vscophoto', u'vscom', u'vscocamera', u'vscoawar', u'vscostyle', u'vscovibe', u'vscophil',
     u'vscosummer', u'instavsco', u'vscogrid', u'vsco_food', u'vscolove', u'vscolover', u'vscovsco',
-    u'vscogang',
+    u'vscogang', u'vscofeature',
     u'follow', u'followme', u'followmeback', u'followhim', u'followher', u'photooftheday',
     u'instamood', u'instagood', u'instagram', u'instatags4likes', u'likeforlike', u'like4like', u'instacollage',
     u'tagsforlikes']
