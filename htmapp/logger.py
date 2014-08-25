bac4623e5ac2ecadd0289a99ba3429f81a214399
p.devaikin@gmail.com <p.devaikin@gmail.com>
2014-08-25 17:19:35 +0400

    configs from env'

--- htmapp/logger.py
+++ htmapp/logger.py
@@ -11,15 +11,17 @@ def get_logger():
     return current_app.logger
 
 def set_logger_params(app):
-    f = DebugFilter()
-    debug_handler = TimedRotatingFileHandler(app.config['LOG_FILE_DEBUG'], when='D', interval=1)
-    debug_handler.addFilter(f)
-    debug_handler.setLevel(logging.DEBUG)
-    debug_handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
-    app.logger.addHandler(debug_handler)
+    if app.config['LOG_FILE_DEBUG'] != None:
+        f = DebugFilter()
+        debug_handler = TimedRotatingFileHandler(app.config['LOG_FILE_DEBUG'], when='D', interval=1)
+        debug_handler.addFilter(f)
+        debug_handler.setLevel(logging.DEBUG)
+        debug_handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+        app.logger.addHandler(debug_handler)
 
-    handler = TimedRotatingFileHandler(app.config['LOG_FILE'], when='D', interval=1)
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
-    app.logger.addHandler(handler)
+    if app.config['LOG_FILE'] != None:
+        handler = TimedRotatingFileHandler(app.config['LOG_FILE'], when='D', interval=1)
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
+        app.logger.addHandler(handler)
 
--- htmapp/web/application_factory.py
+++ htmapp/web/application_factory.py
@@ -3,12 +3,14 @@ from flask import Flask
 from htmapp.web.app import htm_app
 from htmapp.logger import set_logger_params
 from htmapp.db.db_engine import init_db
+import os
 
 # configure
 def create_app(config_object=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('htmapp.default_settings')
     app.config.from_pyfile('application.cfg', silent=True)
+    app.config.from_pyfile(os.environ['HASHTAGMAP_SETTINGS'], silent=True)
 
     if config_object != None:
         app.config.from_object(config_object)
