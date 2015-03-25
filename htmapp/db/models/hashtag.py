63a8a06445faccf79bfeb14e6c04213a6e1fdd11
p.devaikin@gmail.com <p.devaikin@gmail.com>
2015-03-25 15:37:23 +0300

    unique

--- htmapp/db/models/hashtag.py
+++ htmapp/db/models/hashtag.py
@@ -3,7 +3,7 @@ from htmapp.db.db_engine import get_db
 from peewee import *
 
 class Hashtag(Model):
-    name = CharField()
+    name = CharField(unique=True)
 
     class Meta:
         database = get_db()
